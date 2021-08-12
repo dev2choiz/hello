e9462e34b411f25e6c7d263cf9b111e9230b4c8e
dev2choiz <dev2choiz@gmail.com>
2021-08-12 21:42:37 +0200

    Update logs to stackdriver

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -18,11 +18,38 @@ func init() {
 		config = zap.NewProductionConfig()
 	}
 	config.Level.SetLevel(zap.InfoLevel)
+	configureForStackDriver(&config)
 	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	inst, _ = config.Build()
 	defer inst.Sync() // flushes buffer, if any
-	inst = inst.With(zap.Namespace("hello-api"))
+	inst = inst.WithOptions(zap.AddCallerSkip(1))
+	inst = inst.With(zap.Namespace("more"))
 	sugarInst = inst.Sugar()
+}
+
+func configureForStackDriver(conf *zap.Config) {
+	conf.EncoderConfig.LevelKey = "severity"
+	conf.EncoderConfig.MessageKey = "message"
+	conf.EncoderConfig.TimeKey = "time"
+	conf.EncoderConfig.CallerKey = "logging.googleapis.com/sourceLocation"
+	conf.EncoderConfig.EncodeLevel = func(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+		switch l {
+		case zapcore.DebugLevel:
+			enc.AppendString("DEBUG")
+		case zapcore.InfoLevel:
+			enc.AppendString("INFO")
+		case zapcore.WarnLevel:
+			enc.AppendString("WARNING")
+		case zapcore.ErrorLevel:
+			enc.AppendString("ERROR")
+		case zapcore.DPanicLevel:
+			enc.AppendString("CRITICAL")
+		case zapcore.PanicLevel:
+			enc.AppendString("ALERT")
+		case zapcore.FatalLevel:
+			enc.AppendString("EMERGENCY")
+		}
+	}
 }
 
 func Info(msg string, fields ...zap.Field) {
