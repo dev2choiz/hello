336f5756bb0b5c6ef2151ee6d30ccae5b652a2d7
dev2choiz <dev2choiz@gmail.com>
2021-08-15 17:05:45 +0200

    Optimize log

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -135,17 +135,19 @@ func (enc gcpEncoder) EncodeEntry(ent zapcore.Entry, fields []zapcore.Field) (*b
 	if idx != -1 {
 		file = file[:idx]
 	}
+
 	chunk := []byte(fmt.Sprintf(
 		",\"logging.googleapis.com/sourceLocation\":{\"file\":\"%s\",\"line\":\"%d\"}",
 		file, ent.Caller.Line))
 
 	buf, err := enc.Encoder.EncodeEntry(ent, fields)
 	cop := buf.Bytes()
-	final := []byte{}
+	final := make([]byte, 0, len(cop) + len(chunk))
 	final = append(final, cop[:len(cop) - 2]...)
 	final = append(final, chunk...)
 	final = append(final, cop[len(cop) - 2:]...)
 	buf.Reset()
-	buf.Write(final)
+	_, _ = buf.Write(final)
+
 	return buf, err
 }
