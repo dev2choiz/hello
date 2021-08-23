085f74ff9bed1bb31897e18690d4503e9ccd0859
dev2choiz <dev2choiz@gmail.com>
2021-08-23 16:55:06 +0200

    Load tests + adjust resources

---
+++ load-tests/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/bojand/ghz/printer"
+	"github.com/bojand/ghz/runner"
+)
+
+const rootCertPath = "./../local/files/cert/client.pem"
+const protoPath = "./../api/proto"
+const thirdPartyPath = "./../third_party"
+
+const url = "dev2choiz.com:443"
+
+func main() {
+	report, err := runner.Run(
+		"hello.endpoints.health.Health.Status",
+		url,
+		runner.WithProtoFile("health.proto", []string{thirdPartyPath, protoPath}),
+		runner.WithDataFromFile("data.json"),
+		runner.WithRootCertificate(rootCertPath),
+		// test config
+		runner.WithConcurrency(500),
+		runner.WithRunDuration(time.Duration(120) * time.Second),
+		runner.WithStreamInterval(time.Duration(250) * time.Millisecond),
+		runner.WithStreamCallCount(4),
+		//runner.WithStreamCallDuration(time.Duration(1) * time.Second),
+	)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	const logPath = "./report.html"
+	_ = os.Remove(logPath)
+	f, err := os.Create(logPath)
+	printer := printer.ReportPrinter{Report: report }
+
+	printer.Out = os.Stdout
+	printer.Print("summary")
+
+	printer.Out = f
+	printer.Print("html")
+}
