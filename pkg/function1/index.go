93a17b1749fd4ec515490bb8fb1310ec5fa456e3
dev2choiz <dev2choiz@gmail.com>
2021-09-23 15:52:03 +0200

    WIP: remove waiting in the cloud function

--- pkg/function1/index.go
+++ pkg/function1/index.go
@@ -5,14 +5,10 @@ import (
 	"github.com/dev2choiz/hello/pkg/protobuf/healthpb"
 	"github.com/dev2choiz/hello/pkg/version"
 	"go.uber.org/zap"
-	"time"
 )
 
 func DoFunction1(name string) error {
-	sec := 20
 	logger.Info("hello "+name, zap.String("version", version.Get()))
-	<-time.After(time.Duration(sec) * time.Second)
-	logger.Info("bye " + name)
 	test := healthpb.StatusResponse{}
 	logger.Infof("pb: %v", test)
 
