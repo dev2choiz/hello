d239036abb3621e2d2751ddef746ed0c9b2a1907
dev2choiz <dev2choiz@gmail.com>
2021-09-24 11:20:07 +0200

    Use common.groovy in jenkins files

--- deployments/jenkins/common.groovy
+++ deployments/jenkins/common.groovy
@@ -1,6 +1,5 @@
 import groovy.transform.Field
 
-//def repoURL = 'git@github.com:dev2choiz/hello.git'
 @Field
 def helmTag = '3.2.4'
 @Field
@@ -10,12 +9,16 @@ def projectId = "samyn-project4"
 @Field
 def gcloudCredentialsId = projectId
 @Field
+def gcpRegion    = "europe-west1"
+@Field
 def clusterZone = "europe-west1-d"
 @Field
 def clusterName = "samyn-cluster"
 @Field
 def namespace   = "lazone"
 @Field
-def environment = "staging"
+def kmsKeyringName = "kr-hello"
+@Field
+def kmsKeyName = "key-default-hello"
 
 return this
