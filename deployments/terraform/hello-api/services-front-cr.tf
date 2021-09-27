4858c4a17f8e88b8455154f7a53720bfd5968518
dev2choiz <dev2choiz@gmail.com>
2021-09-27 08:42:48 +0200

    Enable cloud cdn + https redirection

--- deployments/terraform/hello-api/services-front-cr.tf
+++ deployments/terraform/hello-api/services-front-cr.tf
@@ -21,6 +21,20 @@ resource "google_compute_backend_service" "front_cr_backend_service" {
     load_balancing_scheme = "EXTERNAL"
     protocol = "HTTPS"
 
+    enable_cdn = true
+    cdn_policy {
+        default_ttl = 3600
+        cache_mode = "CACHE_ALL_STATIC"
+        signed_url_cache_max_age_sec = 3600
+        cache_key_policy {
+            include_query_string = true
+        }
+    }
+    custom_response_headers = [
+        "X-Cache-Status: {cdn_cache_status}",
+        "X-Cache-ID: {cdn_cache_id}",
+    ]
+
     backend {
         // add the NEG to the backend service
         group = google_compute_region_network_endpoint_group.front_cr_neg.id
@@ -29,7 +43,7 @@ resource "google_compute_backend_service" "front_cr_backend_service" {
 
 resource "google_compute_url_map" "front_cr_url_map" {
     provider = google-beta
-    name = "front-cr-url-map"
+    name = "front-cr-lb"
     default_service = google_compute_backend_service.front_cr_backend_service.id
 }
 
@@ -47,6 +61,32 @@ resource "google_compute_global_forwarding_rule" "front_cr_forwarding_rule" {
     target = google_compute_target_https_proxy.front_cr_target_https_proxy.id
     ip_address = google_compute_global_address.front_cr_address.address
     port_range = "443"
+}
+
+// http redirect rule
+resource "google_compute_url_map" "front_cr_url_map_http_redirect" {
+    provider = google-beta
+    name = "front-cr-lb-http-redirect"
+    default_url_redirect {
+        strip_query = false
+        https_redirect = true
+        redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"
+    }
+}
+
+resource "google_compute_target_http_proxy" "front_cr_target_http_proxy" {
+    provider = google-beta
+    name = "front-cr-target-http-proxy"
+    url_map = google_compute_url_map.front_cr_url_map_http_redirect.id
+}
+
+resource "google_compute_global_forwarding_rule" "front_cr_forwarding_rule_http" {
+    provider = google-beta
+    name = "front-cr-forwarding-rule-http"
+    load_balancing_scheme = "EXTERNAL"
+    target = google_compute_target_http_proxy.front_cr_target_http_proxy.id
+    ip_address = google_compute_global_address.front_cr_address.address
+    port_range = "80"
 }
 
 // Get cloud run data (deployed in the ci/cd pipeline)
