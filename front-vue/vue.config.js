09bc7482dd98a12c46cf434ef0543ac47116bf87
dev2choiz <dev2choiz@gmail.com>
2021-10-20 11:16:08 +0200

    Init vue 3 app

---
+++ front-vue/vue.config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
+
+module.exports = {
+  chainWebpack: conf => {
+    conf.resolve.alias.set('@components', resolve('src/component'))
+  }
+}
