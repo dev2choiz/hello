0a3998ba6e61966c9ba9afc528dcd852e405740e
dev2choiz <dev2choiz@gmail.com>
2021-10-22 14:22:21 +0200

    Vue config type

--- front-vue/vue.config.js
+++ front-vue/vue.config.js
@@ -1,11 +1,14 @@
 const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
 module.exports = {
     chainWebpack: conf => {
         conf.resolve.alias.set('@components', resolve('src/components'))
         conf.resolve.alias.set('@pages', resolve('src/pages'))
         conf.resolve.alias.set('@protobuf', resolve('src/protobuf'))
         conf.resolve.alias.set('@services', resolve('src/services'))
-    }
+    },
 }
